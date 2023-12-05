4ae53dc90c27249467714dd1a147c79be0b0029f
Erik Schneider <erik.schneider.pacheco@gmail.com>
2023-12-05 13:31:59 -0300

    Ideia numero 3 adicionada

--- SQL/consultas_de_negocio.sql
+++ SQL/consultas_de_negocio.sql
@@ -18,3 +18,25 @@ SELECT
     corretor
 FROM mydb.locacao
 WHERE vigencia < CURRENT_DATE;
+
+-- Listar imóveis disponíveis para aluguel
+SELECT
+        i.idimovel,
+        l.cep,
+        l.numero_imovel,
+        l.complemento,
+        l.referencia,
+        l.cidade,
+        c.nome AS categoria,
+        i.proprietario,
+        i.preco_aluguel
+    FROM
+        imovel i
+    LEFT JOIN
+        locacao lo ON i.idimovel = lo.imovel
+    JOIN
+        localizacao l ON i.localizacao = l.idlocalizacao
+    JOIN
+        categoria c ON l.categoria = c.idcategoria
+    WHERE
+        lo.imovel IS NULL;
