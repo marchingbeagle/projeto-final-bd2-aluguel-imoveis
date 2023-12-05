4455c058113eeaefe7db7163b983d9691eda2008
Erik Schneider <erik.schneider.pacheco@gmail.com>
2023-12-05 14:36:34 -0300

    triggers fixed

--- SQL/trigger.sql
+++ SQL/trigger.sql
@@ -1,5 +1,4 @@
-DELIMITER //
-
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_pessoas BEFORE DELETE ON pessoas FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -7,8 +6,9 @@ CREATE TRIGGER deletar_registro_log_pessoas BEFORE DELETE ON pessoas FOR EACH RO
         JSON_OBJECT('idpessoas', OLD.idpessoas, 'cpf', OLD.cpf, 'data_nascimento', OLD.data_nascimento, 'endereco', OLD.endereco, 'telefone', OLD.telefone, 'sexo', OLD.sexo),
         NOW()
     );
-    DELETE FROM pessoas WHERE idpessoas = OLD.idpessoas; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_locacao BEFORE DELETE ON locacao FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -16,8 +16,9 @@ CREATE TRIGGER deletar_registro_log_locacao BEFORE DELETE ON locacao FOR EACH RO
         JSON_OBJECT('idlocacao', OLD.idlocacao, 'inquilino', OLD.inquilino, 'valor_contrato', OLD.valor_contrato, 'vigencia', OLD.vigencia, 'localizacao', OLD.localizacao, 'corretor', OLD.corretor, 'idimovel', OLD.idimovel),
         NOW()
     );
-    DELETE FROM locacao WHERE idlocacao = OLD.idlocacao; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_localizacao BEFORE DELETE ON localizacao FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -25,8 +26,9 @@ CREATE TRIGGER deletar_registro_log_localizacao BEFORE DELETE ON localizacao FOR
         JSON_OBJECT('idlocalizacao', OLD.idlocalizacao, 'cep', OLD.cep, 'numero_imovel', OLD.numero_imovel, 'complemento', OLD.complemento, 'referencia', OLD.referencia, 'cidade', OLD.cidade, 'categoria', OLD.categoria),
         NOW()
     );
-    DELETE FROM localizacao WHERE idlocalizacao = OLD.idlocalizacao; END;
+    END&&;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_categoria BEFORE DELETE ON categoria FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -34,8 +36,9 @@ CREATE TRIGGER deletar_registro_log_categoria BEFORE DELETE ON categoria FOR EAC
         JSON_OBJECT('idcategoria', OLD.idcategoria, 'nome', OLD.nome, 'descricao', OLD.descricao, 'data_criacao', OLD.data_criacao, 'data_atualizacao', OLD.data_atualizacao),
         NOW()
     );
-    DELETE FROM categoria WHERE idcategoria = OLD.idcategoria; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_cidade BEFORE DELETE ON cidade FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -43,8 +46,9 @@ CREATE TRIGGER deletar_registro_log_cidade BEFORE DELETE ON cidade FOR EACH ROW 
         JSON_OBJECT('idcidade', OLD.idcidade, 'nome', OLD.nome, 'estado', OLD.estado),
         NOW()
     );
-    DELETE FROM cidade WHERE idcidade = OLD.idcidade; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_imovel BEFORE DELETE ON imovel FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -52,8 +56,9 @@ CREATE TRIGGER deletar_registro_log_imovel BEFORE DELETE ON imovel FOR EACH ROW 
         JSON_OBJECT('idimovel', OLD.idimovel, 'localizacao', OLD.localizacao, 'proprietario', OLD.proprietario, 'preco_compra', OLD.preco_compra, 'preco_aluguel', OLD.preco_aluguel),
         NOW()
     );
-    DELETE FROM imovel WHERE idimovel = OLD.idimovel; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_estado BEFORE DELETE ON estado FOR EACH ROW BEGIN
     INSERT INTO log(titulo, descricao, data)
     VALUES (
@@ -61,8 +66,9 @@ CREATE TRIGGER deletar_registro_log_estado BEFORE DELETE ON estado FOR EACH ROW 
         JSON_OBJECT('idestado', OLD.idestado, 'nome', OLD.nome, 'sigla', OLD.sigla),
         NOW()
     );
-    DELETE FROM estado WHERE idestado = OLD.idestado; END;
+    END$$;
 
+DELIMITER $$
 CREATE TRIGGER deletar_registro_log_log
 BEFORE DELETE ON log
 FOR EACH ROW
@@ -73,6 +79,4 @@ BEGIN
         JSON_OBJECT('idlog', OLD.idlog, 'titulo', OLD.titulo, 'descricao', OLD.descricao, 'data', OLD.data),
         NOW()
     );
-END;
-
-DELIMITER ;
+END$$;
