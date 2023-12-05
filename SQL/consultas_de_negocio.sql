adf99f2075fb1b939b57a558cda1c3b322e391db
Erik Schneider <erik.schneider.pacheco@gmail.com>
2023-12-04 22:20:23 -0300

    Consulta de negócio adicionada

--- SQL/consultas_de_negocio.sql
+++ SQL/consultas_de_negocio.sql
@@ -6,3 +6,15 @@ FROM mydb.imovel i
 INNER JOIN mydb.localizacao l ON i.localizacao = l.idlocalizacao
 INNER JOIN mydb.categoria c ON l.categoria = c.idcategoria
 GROUP BY c.nome;
+
+-- Listar todos os contratos de locação com vigência expirada:
+SELECT
+    idlocacao,
+    inquilino,
+    valor_contrato,
+    vigencia,
+    CURRENT_DATE AS data_atual,
+    imovel,
+    corretor
+FROM mydb.locacao
+WHERE vigencia < CURRENT_DATE;
